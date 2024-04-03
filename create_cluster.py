e18ae56988028cf314548712348dbc98b0a04298
opilonelson <30505192+opilonelson@users.noreply.github.com>
2024-04-03 15:11:49 +0300

    Update create_cluster.py

--- create_cluster.py
+++ create_cluster.py
@@ -4,7 +4,7 @@ eks_client = boto3.client('eks')
 #replace name with the desired cluster name
 #replace the subnet ids with actual subnet ids from the vpc where the cluster is to be created
 #replace the security group with actual security group in the relevant vpc 
-response=eks_client.create_cluster(name='nellz-demo-cluster', roleArn='arn:aws:iam::987654321098:role/eks-cluster-role', resourcesVpcConfig={'subnetIds':['subnet-4567de56', 'sg-6587jh42'], 'securityGroupIds':['sg-1234fe89'])
+response=eks_client.create_cluster(name='nellz-demo-cluster', roleArn='arn:aws:iam::987654321098:role/eks-cluster-role', resourcesVpcConfig={'subnetIds':['subnet-4567de56', 'subnet-6587jh42'], 'securityGroupIds':['sg-1234fe89'])
 print(response['cluster']['status'])
 
 
